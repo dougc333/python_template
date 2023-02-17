7ee0a6d6b6d1932a28361e00531f443600c046f3
doug chang <dougc@stanfordalumni.org>
2023-02-17 05:01:40 +0000

    fix ff

--- test/test_logic.py
+++ test/test_logic.py
@@ -1,5 +1,8 @@
-from mylib.logic import wiki
+from mylib.logic import wiki, search_wiki
 
 
 def test_wiki():
-    assert("god" in wiki())
+    assert("Barack" in wiki("Barack Obama"))
+
+def test_search():
+    assert("Barack Obama" in search_wiki("Barack"))
