fd50048982207e212fcb318a1412bcfeaca570fd
doug chang <dougc@stanfordalumni.org>
2023-02-17 04:15:37 +0000

    asdf

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 from mylib.logic import wiki
+from fastapi import fastapi
+import uvicorn
+
+app = fastapi()
+
+
+@app.get("/")
+async def root():
+    return {"message":"hello"}
+
+@app.get("/add/{num1}/{num2}")
+async def add(num1:int, num2:int):
+    """add 2 numbers"""
+    return {"total": num1+num2}
+
+
+
 
 print(wiki())
